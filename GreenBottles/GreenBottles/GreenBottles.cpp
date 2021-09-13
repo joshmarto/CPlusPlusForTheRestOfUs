8059610fe76f2308264e6a6fdd469955d48c7948
joshmarto <65306069+joshmarto@users.noreply.github.com>
2021-09-13 14:15:21 -0600

    Project

---
+++ GreenBottles/GreenBottles/GreenBottles.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int greenBottles = 10;
+    std::cout << "There were " << greenBottles << " green bottles\n";
+    greenBottles--;
+    std::cout << "There were " << greenBottles << " green bottles\n";
